04330ed952b23f69b3395e166df9b90fafcf416d
Lars Antonio Kay <antlak@ANT-PC-LAK.(none)>
2013-05-30 15:58:36 +0200

    Added create action

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,1 +1,15 @@
-creating_projects_spec.rb
+require 'spec_helper'
+
+feature 'Creating Projects' do
+  scenario "can create a project" do
+    visit '/'
+
+    click_link 'New Project'
+
+    fill_in 'Name', with: 'TextMate 2'
+    fill_in 'Description', with: 'A text-editor for OS X'
+    click_button 'Create Project'
+
+    expect(page).to have_content('Project has been created.')
+  end
+end
